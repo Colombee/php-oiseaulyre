3f5151d0be867efa320762306772f9d0de137be4
Colombe <c.schh@outlook.com>
2018-09-12 17:06:32 +0200

    mvc organization

--- view/login.php
+++ view/login.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/CSS" href="../style.css">
+    <link rel="stylesheet" type="text/CSS" href="../assets/css/style.css">
+    <link rel="stylesheet" type="text/CSS" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    
     <title>Connexion</title>
 </head>
 <body>
-    <img class="login-logo" src="../assets/oiseau-lyre-logo.png">
-
+    <img class="login-logo" src="../assets/img/oiseau-lyre-logo.png">
     <section class="login-section">
         <h1 class="login-top-section">Connexion</h1>
         <form>
@@ -21,6 +22,5 @@
             <button class="login-button" type="submit">Se connecter</button>
         </form>
     </section>
-    
 </body>
 </html>
